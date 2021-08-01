5ba1ee9fcedf8a9152c2f17743accf19ef0b5616
AMihaylenko <amikhaylenko@metaprime.ru>
2021-08-02 02:11:59 +0300

    restructured teams stats rows and columns

--- basketball/helpers/teamsStats.js
+++ basketball/helpers/teamsStats.js
@@ -11,17 +11,32 @@ export async function getTeamsStats(page){
   }
   try {
     const rows = await page.$$('#all_team_stats_totals table.sortable.stats_table.now_sortable tbody tr');
+    let array = []
       for (const row of rows) {
-        let mappedRows = await row.$$eval('th, td', (options, table_headers) => options.map( (option, index) => {
+        array.push(await row.$$eval('th, td', (options, table_headers) => options.map( (option, index) => {
           return {
             [table_headers[index]] : option.textContent
           }
-        }), table_headers);
-        rowsValues.push(mappedRows)
+        }), table_headers));
       }
+      let newarr = array.map(x=> {
+        return Object.fromEntries(
+          x.flatMap(item => {
+            return Object.entries(item)
+          })
+          )
+      })
+      rowsValues.push(...newarr)
   } catch (error) {
     console.error(error)
   }
+  // map headers
+  table_headers = table_headers.map(x=> {
+    return {
+      Header: x,
+      accessor: x,
+    }
+  })
   return {
     headers: table_headers,
     rows: rowsValues
