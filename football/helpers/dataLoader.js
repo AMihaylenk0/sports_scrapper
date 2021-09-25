f1d4fb70e9f1f508254b2dde1f20778e32258a11
AMihaylenko <amikhaylenko@metaprime.ru>
2021-09-26 02:01:04 +0300

    football europe cups standings

--- football/helpers/dataLoader.js
+++ football/helpers/dataLoader.js
@@ -76,7 +76,7 @@ export async function fetchFootballData({league, standingsUrl, teamsStatsUrl, pl
     // });
     // await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36');
     // await page.reload({ waitUntil: ["networkidle2"] });
-    let standings = await getFootballStandings(page, standingsUrl)
+    let standings = await getFootballStandings(page, standingsUrl, league)
     let teamsStats = await getFootballTeamsStats(page, teamsStatsUrl)
     let playersStats = await getFootballPlayersStats(page, playersStatsUrl)
     return {
--- football/parsers/standings.js
+++ football/parsers/standings.js
@@ -1,53 +1,121 @@
-export async function getFootballStandings(page, standingsUrl){
+export async function getFootballStandings(page, standingsUrl, league){
   page.setDefaultTimeout(0)
   await page.goto(standingsUrl);
   await page.waitForSelector('.tournament-standings-table');
-  let table_headers = []
-  let rowsValues = []
 
-  try {
-    if (!table_headers.length) {
-      table_headers = await page.$$eval(
-        ".tournament-standings-table [id^='standings'] table thead tr[class*='general-header'] th",
-        (options) => options.map((option) => option.textContent)
+  if (league === 'champions_league' || league === 'europa_league') {
+    // parse titles
+    try {
+      let titles = []
+      titles = await page.$$eval(
+        ".tournament-tables-header",
+        (options) => options.map((option) => option.textContent.trim())
       )
-    }
-  } catch (error) {
-    console.error(error)
-  }
-  try {
-    const rows = await page.$$('.tournament-standings-table table[id*="standings"] tbody tr');
-    let array = []
-      for (const row of rows) {
-        array.push(await row.$$eval('th, td', (options, table_headers) => options.map( (option, index) => {
+      
+      // parse groups    
+      const groupTables = await page.$$('.tournament-standings-table table[id^="standings"]');
+      let groups = []
+      let table_headers = []
+
+      for (const table of groupTables) {
+        // getting tables
+        if (!table_headers.length) {
+          table_headers = await table.$$eval(
+            "thead tr[class*='general-header'] th",
+            (options) => options.map((option) => option.textContent)
+          )
+        }
+        // getting rows
+        const rows = await table.$$('tbody tr');
+        let array = []
+
+        for (const row of rows) {
+          array.push(await row.$$eval('th, td', (options, table_headers) => options.map( (option, index) => {
+            return {
+              [table_headers[index]] : option.textContent
+            }
+          }), table_headers));
+        }
+        let table_rows = array.flatMap((x,i)=>{
+          return Object.assign({}, ...x)
+        })
+        table_rows = table_rows.map(x=> {
           return {
-            [table_headers[index]] : option.textContent
+            ...x,
+            'Команда': formatString(x['Команда'])
           }
-        }), table_headers));
+        })
+        groups.push({
+          headers: table_headers.map(x=> {
+            return {
+              Header: x,
+              accessor: x,
+            }
+          }),
+          rows: table_rows
+        })
       }
-      let newarr = array.flatMap((x,i)=>{
-        return Object.assign({}, ...x)
-      })
-      newarr = newarr.map(x=> {
+
+      // merge titles and tables
+      let data = titles.map( (x,i) => {
         return {
-          ...x,
-          'Команда': formatString(x['Команда'])
+          title: x,
+          group: groups[i]
         }
       })
-      rowsValues.push(...newarr)
-  } catch (error) {
-    console.error(error)
-  }
-   // map headers
-   table_headers = table_headers.map(x=> {
-    return {
-      Header: x,
-      accessor: x,
+
+      return data
+
+    } catch (error) {
+      console.error(error)
     }
-  })
-  return {
-    headers: table_headers,
-    rows: rowsValues
+  } else {
+    let table_headers = []
+    let rowsValues = []
+    try {
+      if (!table_headers.length) {
+        table_headers = await page.$$eval(
+          ".tournament-standings-table [id^='standings'] table thead tr[class*='general-header'] th",
+          (options) => options.map((option) => option.textContent)
+        )
+      }
+    } catch (error) {
+      console.error(error)
+    }
+    try {
+      const rows = await page.$$('.tournament-standings-table table[id*="standings"] tbody tr');
+      let array = []
+        for (const row of rows) {
+          array.push(await row.$$eval('th, td', (options, table_headers) => options.map( (option, index) => {
+            return {
+              [table_headers[index]] : option.textContent
+            }
+          }), table_headers));
+        }
+        let newarr = array.flatMap((x,i)=>{
+          return Object.assign({}, ...x)
+        })
+        newarr = newarr.map(x=> {
+          return {
+            ...x,
+            'Команда': formatString(x['Команда'])
+          }
+        })
+        rowsValues.push(...newarr)
+    } catch (error) {
+      console.error(error)
+    }
+     // map headers
+     table_headers = table_headers.map(x=> {
+      return {
+        Header: x,
+        accessor: x,
+      }
+    })
+    return {
+      headers: table_headers,
+      rows: rowsValues
+    }
   }
 }
 function formatString(str){
